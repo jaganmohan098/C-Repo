d87fd2bad92d58191f69ea2ff847f83ab95fb38e
jaganmohan098 <24B11EC098@adityauniversity.in>
2024-10-07 21:37:01 +0530

    Uploading code: Alpha Trouble.c

---
+++ Alpha Trouble.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+int main(){
+    char ja;
+    scanf("%ch",&ja);
+    if(ja=='a'||ja=='A'){
+        printf("1");
+    }
+    else if(ja=='b'||ja=='B'){
+        printf("2");
+    }
+    else if(ja=='c'||ja=='C'){
+        printf("3");
+    }
+    else if(ja=='d'||ja=='D'){
+        printf("4");
+    }
+    else if(ja=='e'||ja=='E'){
+        printf("5");
+    }
+    else if(ja=='f'||ja=='F'){
+        printf("6");
+    }
+    else if(ja=='g'||ja=='G'){
+        printf("7");
+    }
+    else if(ja=='h'||ja=='H'){
+        printf("8");
+    }
+    else if(ja=='i'||ja=='I'){
+        printf("9");
+    }
+    else if(ja=='j'||ja=='J'){
+        printf("10");
+    }
+    else if(ja=='k'||ja=='K'){
+        printf("11");
+    }
+    else if(ja=='l'||ja=='L'){
+        printf("12");
+    }
+    else if(ja=='m'||ja=='M'){
+        printf("13");
+    }
+    else if(ja=='n'||ja=='N'){
+        printf("14");
+    }
+    else if(ja=='o'||ja=='O'){
+        printf("15");
+    }
+    else if(ja=='p'||ja=='P'){
+        printf("16");
+    }
+    else if(ja=='q'||ja=='Q'){
+        printf("17");
+    }
+    else if(ja=='r'||ja=='R'){
+        printf("18");
+    }
+    else if(ja=='s'||ja=='S'){
+        printf("19");
+    }
+    else if(ja=='t'||ja=='T'){
+        printf("20");
+    }
+    else if(ja=='u'||ja=='U'){
+        printf("21");
+    }
+    else if(ja=='v'||ja=='V'){
+        printf("22");
+    }
+    else if(ja=='w'||ja=='W'){
+        printf("23");
+    }
+    else if(ja=='x'||ja=='X'){
+        printf("24");
+    }
+    else if(ja=='y'||ja=='Y'){
+        printf("25");
+    }
+    else {
+        printf("26");
+    }
+}
